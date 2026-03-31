3eae1d0baac76e0eb98d026d77caf616dd7a8906
Fahad Ali <fahadj698@gmail.com>
2026-04-01 01:57:30 +0500

    Update form.tsx

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+"use client";
+
 import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
-import  { Slot } from "@radix-ui/react-slot";
+import { Slot } from "@radix-ui/react-slot";
+
 import {
   Controller,
   FormProvider,
