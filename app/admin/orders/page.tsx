9f9683dad4f4c7062202f84bf1f63dc59faa4518
Fahad Ali <fahadj698@gmail.com>
2026-04-29 22:07:34 +0500

    Delete app directory

--- app/admin/orders/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LiveOrderFeed } from "@/components/admin/LiveOrderFeed";
-
-export default function AdminOrdersPage() {
-  return (
-    <div className="flex flex-col gap-6">
-      <LiveOrderFeed />
-    </div>
-  );
-}
--- app/admin/products/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ProductsTable } from "@/components/admin/ProductsTable";
-
-export default function AdminProductsPage() {
-  return (
-    <div className="flex flex-col gap-6">
-      <ProductsTable />
-    </div>
-  );
-}
